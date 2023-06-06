10cf845147f8739a54dc864a69fd2ed753defe08
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2023-06-06 09:01:50 -0400

    updated theme and colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' data-theme='forest'>
       <body className='mx-4 lg:mx-44 '>
         <Nav />
         {children}
--- app/sports/[id]/page.tsx
+++ app/sports/[id]/page.tsx
@@ -4,7 +4,14 @@ export default async function SportsPage({ searchParams }) {
   // const [oddsData, setOddsData] = useState<null | OddsData[]>(null);
   // const [error, setError] = useState<null | string>(null);
   const id = searchParams.id;
-  console.log(id);
+  const sportsbooks = [
+    "DraftKings",
+    "BetMGM",
+    "MyBookie.ag",
+    "FanDuel",
+    "Barstool Sportsbook",
+  ];
+
   const fetchData = async () => {
     console.log(id);
     if (id) {
@@ -28,61 +35,63 @@ export default async function SportsPage({ searchParams }) {
   // render your oddsData here
 
   return (
-    <div className='max-w-screen mx-auto px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
+    <div className='max-w-screen mx-auto px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 text-white'>
       {oddsData &&
         oddsData.map((odd) => (
-          <div key={odd.id} className='card bordered bg-slate-600'>
+          <div key={odd.id} className='card bordered bg-secondary'>
             <div className='card-body'>
               <h2 className='card-title'>
                 {odd.home_team} vs {odd.away_team}
               </h2>
-              {odd.bookmakers.map((book) => (
-                <div key={book.key}>
-                  <h3 className='font-bold text-lg'>{book.title}</h3>
-                  {book.markets.map((market) => (
-                    <div key={market.key}>
-                      <h4 className=''>
-                        {market.key == "h2h" ? "Moneyline" : "Spread"}
-                      </h4>
-                      <table className='table-auto w-full text-center border-collapse border border-gray-300'>
-                        <thead>
-                          <tr className='grid grid-cols-3 divide-x divide-gray-300'>
-                            <th className='p-2 border-b border-gray-300'>
-                              Name
-                            </th>
-                            <th className='p-2 border-b border-gray-300'>
-                              Price
-                            </th>
-                            <th className='p-2 border-b border-gray-300'>
-                              Point
-                            </th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {market.outcomes.map((outcome) => (
-                            <tr
-                              key={outcome.name}
-                              className='grid grid-cols-3 divide-x divide-gray-300'
-                            >
-                              <td className='p-2 border-b border-gray-300'>
-                                {outcome.name}
-                              </td>
-                              <td className='p-2 border-b border-gray-300'>
-                                {outcome.price}
-                              </td>
-                              <td className='p-2 border-b border-gray-300'>
-                                {outcome.point != null
-                                  ? `Points: ${outcome.point}`
-                                  : "N/A"}
-                              </td>
+              {odd.bookmakers.map((book) =>
+                sportsbooks.includes(book.title) ? (
+                  <div key={book.key}>
+                    <h3 className='font-bold text-lg'>{book.title}</h3>
+                    {book.markets.map((market) => (
+                      <div key={market.key}>
+                        <h4 className=''>
+                          {market.key == "h2h" ? "Moneyline" : "Spread"}
+                        </h4>
+                        <table className='table-auto w-full text-center border-collapse border border-gray-300'>
+                          <thead>
+                            <tr className='grid grid-cols-3 divide-x divide-gray-300'>
+                              <th className='p-2 border-b border-gray-300'>
+                                Name
+                              </th>
+                              <th className='p-2 border-b border-gray-300'>
+                                Price
+                              </th>
+                              <th className='p-2 border-b border-gray-300'>
+                                Point
+                              </th>
                             </tr>
-                          ))}
-                        </tbody>
-                      </table>
-                    </div>
-                  ))}
-                </div>
-              ))}
+                          </thead>
+                          <tbody>
+                            {market.outcomes.map((outcome) => (
+                              <tr
+                                key={outcome.name}
+                                className='grid grid-cols-3 divide-x divide-gray-300'
+                              >
+                                <td className='p-2 border-b border-gray-300'>
+                                  {outcome.name}
+                                </td>
+                                <td className='p-2 border-b border-gray-300'>
+                                  {outcome.price}
+                                </td>
+                                <td className='p-2 border-b border-gray-300'>
+                                  {outcome.point != null
+                                    ? `Points: ${outcome.point}`
+                                    : "N/A"}
+                                </td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    ))}
+                  </div>
+                ) : null
+              )}
             </div>
           </div>
         ))}
