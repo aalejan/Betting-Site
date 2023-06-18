42e6ac9809d7816d6a1afca7260cf775eb525f2f
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2023-06-18 10:58:22 -0400

    updated nav bar to use user google image. Google image is a dropdown that links to dashboard and logout

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,14 +2,21 @@
 "use client";
 import { Session } from "next-auth";
 import { signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Nav({ user }: Session) {
-  const sports = ["mma_mixed_martial_arts", "baseball_mlb", "basketball_nba"];
+  const sports = [
+    "mma_mixed_martial_arts",
+    "baseball_mlb",
+    "basketball_nba",
+    "soccer_usa_mls",
+  ];
   const sportKey: { [key: string]: string } = {
     mma_mixed_martial_arts: "MMA",
     baseball_mlb: "MLB",
     basketball_nba: "NBA",
+    soccer_usa_mls: "MLS",
   };
 
   return (
@@ -33,12 +40,31 @@ export default function Nav({ user }: Session) {
         ))}
       </ul>
       {user && (
-        <button
-          onClick={() => signOut()}
-          className='btn-primary rounded-md p-4'
-        >
-          Sign out
-        </button>
+        <div className='dropdown dropdown-end'>
+          <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
+            <div className='w-10 rounded-full'>
+              <Image
+                src={user?.image as string}
+                alt={user.name as string}
+                width={36}
+                height={36}
+                className='rounded-full'
+                tabIndex={0}
+              />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className='mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52 font-semibold'
+          >
+            <li>
+              <Link href={"/dashboard"}>Dashboard</Link>
+            </li>
+            <li>
+              <a onClick={() => signOut()}>Logout</a>
+            </li>
+          </ul>
+        </div>
       )}
 
       {!user && (
