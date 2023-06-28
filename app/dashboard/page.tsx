3dea50c68753bcec2017e281f07f03632e0bc3dc
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2023-06-28 00:08:43 -0400

    added filtering of bets by won, lost and in progress bets

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,82 @@
 "use client";
 
-import Bet from "../components/Bet";
+import BetComponent from "../components/BetComponent";
+import { useEffect, useState } from "react";
 import useSwr from "swr";
+import { Bet } from "@prisma/client";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const API = `/api/bets`;
 
 export default function BetsDashboard() {
   const { data: bets, error } = useSwr(API, fetcher);
-  console.log(bets);
+  const [filteredBets, setFilteredBets] = useState<Bet[]>([]);
+
+  useEffect(() => {
+    if (bets) {
+      setFilteredBets(bets);
+    }
+  }, [bets]);
+
+  const filterBets = (filterType) => {
+    let newFilteredBets;
+
+    switch (filterType) {
+      case "won":
+        newFilteredBets = bets?.filter((bet) => bet.betOutcome === "win");
+        break;
+      case "loss":
+        newFilteredBets = bets?.filter((bet) => bet.betOutcome === "loss");
+        break;
+      case "in_progress":
+        newFilteredBets = bets?.filter(
+          (bet) => bet.betStatus === "in_progress"
+        );
+        break;
+      default:
+        newFilteredBets = bets;
+    }
+
+    setFilteredBets(newFilteredBets);
+  };
+
+  if (error) return <div>Error loading bets</div>;
+  if (!filteredBets) return <div>Loading...</div>;
 
   return (
-    <div className='flex flex-col lg:flex-row xl:flex-row items-center lg:items-start xl:items-start flex-wrap justify-center gap-3'>
-      {bets && bets.map((bet) => <Bet bet={bet} />)}
-    </div>
+    <>
+      <div className='flex justify-around p-2'>
+        <button
+          className='btn-primary rounded-md p-2'
+          onClick={() => filterBets("win")}
+        >
+          Won
+        </button>
+        <button
+          className='btn-primary rounded-md p-2'
+          onClick={() => filterBets("loss")}
+        >
+          Lost
+        </button>
+        <button
+          className='btn-primary rounded-md p-2'
+          onClick={() => filterBets("in_progress")}
+        >
+          In Progress
+        </button>
+        <button
+          className='btn-primary rounded-md p-2'
+          onClick={() => setFilteredBets(bets)}
+        >
+          All
+        </button>
+      </div>
+
+      <div className='flex flex-col lg:flex-row xl:flex-row items-center lg:items-start xl:items-start flex-wrap justify-center gap-3'>
+        {filteredBets.map((bet) => (
+          <BetComponent key={bet.id} bet={bet} />
+        ))}
+      </div>
+    </>
   );
 }
